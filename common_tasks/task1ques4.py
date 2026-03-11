fde8f4e3f20b6ad89e5cb3956c5bd263ea184d5d
Abhijith-P-B <abhijithp3116@gmail.com>
2026-03-11 22:04:32 +0530

    Added write ups for the Common tasks

--- common_tasks/task1ques4.py
+++ common_tasks/task1ques4.py
@@ -1,17 +1,19 @@
-word_1=input("First Word:")
-word_2=input("Second_input:")
-lword1=word_1.lower()
-lword2=word_2.lower()
+def check_anagram(word1, word2):
+    lword1 = word1.lower()
+    lword2 = word2.lower()
 
-if len(word_1) != len(word_2):
-    print("Not anagrams")
-    
-else:
-    s1=sorted(lword1)
-    s2=sorted(lword2)
-
-    if s1 == s2:
-        print("They are anagrams")
-    else:
+    if len(word1) != len(word2):
         print("Not anagrams")
-    
+    else:
+        s1 = sorted(lword1)
+        s2 = sorted(lword2)
+
+        if s1 == s2:
+            print("They are anagrams")
+        else:
+            print("Not anagrams")
+
+word1 = input("Enter First Word: ")
+word2 = input("Enter Second Word: ")
+
+check_anagram(word1, word2)
